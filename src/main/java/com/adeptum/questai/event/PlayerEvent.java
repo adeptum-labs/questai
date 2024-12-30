28ae8dd64131eeae2669ac7c028fe4771bb641cd
Adam Waldenberg <adam@adeptum.se>
2024-12-30 17:56:51 +0100

    Remove old unused event class

--- src/main/java/com/adeptum/questai/event/PlayerEvent.java
+++ src/main/java/com/adeptum/questai/event/PlayerEvent.java
@@ -1,5 +1,12 @@
 package com.adeptum.questai.event;
 
-public class PlayerEvent {
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 
+public class PlayerEvent {
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Bukkit.broadcastMessage(event.getPlayer().getDisplayName());
+	}
 }
