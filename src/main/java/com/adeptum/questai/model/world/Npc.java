58079523e5e1f68de5c6456b4459c21b43db2922
Adam Waldenberg <adam@adeptum.se>
2026-04-12 13:05:24 +0200

    Remove dead code and unused enums
    Delete PlayerEvent (never registered) and SpawnLocator (never called).
    Remove duplicate Mood and Personality enums from Npc.

--- src/main/java/com/adeptum/questai/model/world/Npc.java
+++ src/main/java/com/adeptum/questai/model/world/Npc.java
@@ -3,46 +3,10 @@ package com.adeptum.questai.model.world;
 import com.adeptum.questai.model.world.quest.Quest;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
 public class Npc {
-	@RequiredArgsConstructor
-	public enum Mood {
-		BOOKISH("Nerdy & bookish"),
-		BRASH("Brash"),
-		CHATTY("Chatty"),
-		ECCENTRIC("Extremely eccentric"),
-		KINDLY("Very kindly"),
-		MYSTIC("Dark & mysterious"),
-		ROYAL("Snoby & royal"),
-		SHY("Shy & careful"),
-		STOIC("Stoic"),
-		SURLY("Surly");
-
-		@Getter
-		private final String description;
-	}
-
-	@RequiredArgsConstructor
-	public enum Personality {
-		BOOKISH("Nerdy & bookish"),
-		BRASH("Brash"),
-		CHATTY("Chatty"),
-		ECCENTRIC("Extremely eccentric"),
-		KINDLY("Very kindly"),
-		MYSTIC("Dark & mysterious"),
-		ROYAL("Snoby & royal"),
-		SHY("Shy & careful"),
-		STOIC("Stoic"),
-		SURLY("Surly");
-
-		@Getter
-		private final String description;
-	}
-
 	private Quest quest;
 	private String nonsensePhrase;
 	private long timestamp;
